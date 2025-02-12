a5a3d9672b39f26a5453f3d330a48bdf9ea1992b
pete3n <pete3n@protonmail.com>
2025-02-11 23:04:43 -0500

    chore: remove debug entry

--- plugin/nix-prefetch-git.lua
+++ plugin/nix-prefetch-git.lua
@@ -1,3 +1,2 @@
-vim.api.nvim_create_user_command("NPGGetAttrs", require("nix-prefetch-git").get_attrs, {})
 vim.api.nvim_create_user_command("NPGUpdateRepo", require("nix-prefetch-git").update_repo_info, {})
 
