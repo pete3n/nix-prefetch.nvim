28fcb36a09e991b0fa1809a3059a1573ae51ec45
pete3n <pete3n@protonmail.com>
2025-02-16 13:58:12 -0500

    fix: function call for NPUpdateHash

--- plugin/nix-prefetch.lua
+++ plugin/nix-prefetch.lua
@@ -1,2 +1,4 @@
 vim.api.nvim_create_user_command("NPUpdateRepo", require("nix-prefetch").update_repo_info, {})
-vim.api.nvim_create_user_command("NPUpdateHash", require("nix-prefetch").update_repo_info(true), {})
+vim.api.nvim_create_user_command("NPUpdateHash", function()
+  require("nix-prefetch").update_repo_info(true)
+end, {})
