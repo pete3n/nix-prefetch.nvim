0b39ecb1a49a0e937aefdc28a9715b5544c81c27
pete3n <pete3n@protonmail.com>
2025-06-29 12:55:31 -0400

    fix: deprecated report call

--- lua/nix_prefetch/health.lua
+++ lua/nix_prefetch/health.lua
@@ -1,31 +1,28 @@
 ---@module "nix_prefetch.health"
 local health = require("vim.health")
 
+local start = health.start
+local ok = health.ok
+local h_error = health.error
+
 local M = {}
 
 function M.check()
-  health.start("nix-prefetch.nvim Health Check")
-
-  -- Check for external binary: jq
-  if vim.fn.executable("jq") == 1 then
-    health.ok("jq is available in your PATH.")
-  else
-    health.report_error("jq is not available in your PATH. Please install it.")
-  end
+  start("nix-prefetch.nvim Health Check")
 
   -- Check for external binary: nix-prefetch-git
   if vim.fn.executable("nix-prefetch-git") == 1 then
-    health.ok("nix-prefetch-git is available in your PATH.")
+    ok("nix-prefetch-git is available in your PATH.")
   else
-    health.error("nix-prefetch-git is not available in your PATH. Please install it.")
+    h_error("nix-prefetch-git is not available in your PATH. Please install it.")
   end
 
   -- Check for the Treesitter dependency (nvim-treesitter)
-  local ok, _ = pcall(require, "nvim-treesitter")
-  if ok then
-    health.ok("nvim-treesitter is installed.")
+  local ts_ok, _ = pcall(require, "nvim-treesitter")
+  if ts_ok then
+    ok("nvim-treesitter is installed.")
   else
-    health.error("nvim-treesitter is not installed. Please install it.")
+    h_error("nvim-treesitter is not installed. Please install it.")
   end
 end
 
